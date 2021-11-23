05e6ed407415ceb7070cf5adc42a261066a3abbb
antecume547 <79765534+antecume547@users.noreply.github.com>
2021-11-23 22:31:11 +0100

    refractor user form

--- server/djangoapp/forms.py
+++ server/djangoapp/forms.py
@@ -1,6 +1,14 @@
+import re
 from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 class SignupForm(forms.Form):
+    error_message = {
+            'short_password' : '',
+            'wrong_password_format' : ''
+            }
+
     username = forms.CharField(max_length = 30, widget =forms.TextInput(attrs={
         'placeholder' : 'Username',
         'class' : 'form-control'
@@ -14,6 +22,40 @@ class SignupForm(forms.Form):
         'class' : 'form-control'
         }))
     password = forms.CharField(widget = forms.PasswordInput())
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        try:
+            User.objects.get(username = username)
+        except User.DoesNotExist
+        return password
+    
+
+    def clean_password(self):
+        password = self.cleaned_data['password']
+        re_patt_number = r"\D"
+        re_patt_letter = r"\W"
+        
+        if password.length < 9:
+            raise ValidationError(
+                    self.error_message['short_password']
+                    )
+        if re.findall(re_patt_number, password) or re.findall(re_patt_letter, password):
+            raise ValidationError(
+                    self.error_message['wrong_password_format']
+                    )
+        return password
+
+    def save(self, commit=True):
+        user = User.objects.create(
+                self.cleaned_data['username'],
+                self.cleaned_data['first_name'],
+                self.cleaned_data['last_name'],
+                self.cleaned_data['password']
+                )
+        return user
+
+
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length = 30,widget =forms.TextInput(attrs={
