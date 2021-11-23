d3ae5f452c0381bf6bb7bd22d8138224c1e6ef11
antecume547 <79765534+antecume547@users.noreply.github.com>
2021-11-23 15:46:32 +0100

    login,logout and signup

--- server/djangoapp/forms.py
+++ server/djangoapp/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class SignupForm(forms.Form):
-    username = forms.Charfield(max_length = 30)
-    first_name =forms.Charfield(max_length = 60)
-    last_name = forms.Charfield(max_length = 60)
-    password = forms.Charfield(widget = forms.PasswordInput())
+    username = forms.CharField(max_length = 30)
+    first_name =forms.CharField(max_length = 60)
+    last_name = forms.CharField(max_length = 60)
+    password = forms.CharField(widget = forms.PasswordInput())
