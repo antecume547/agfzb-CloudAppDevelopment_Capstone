67f2d511aa14e1dd77f588603d81149424a533c6
antecume547 <79765534+antecume547@users.noreply.github.com>
2021-11-30 20:10:25 +0100

    Login, signup functionality and base tamplete

--- server/djangoapp/forms.py
+++ server/djangoapp/forms.py
@@ -3,74 +3,83 @@ from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
+
 class SignupForm(forms.Form):
     error_message = {
-            'username_duplication' : 'The username is already exists!',
-            'short_password' : 'The pasword should contain at least 9 character!',
-            'wrong_password_format_missing_number' : 'The password should contain at least one number!',
-            'wrong_password_format_missing_letter' : 'The password should contain at least one nonnumeric character!'
-            }
+        "username_duplication": "The username already exists!",
+        "short_password": "The pasword should contain at least 9 character!",
+        "wrong_password_format_missing_number": "The password should contain at least one number!",
+        "wrong_password_format_missing_letter": "The password should contain at least one nonnumeric character!",
+    }
 
-    username = forms.CharField(max_length = 30, widget =forms.TextInput(attrs={
-        'placeholder' : 'Username',
-        'class' : 'form-control'
-        }))
-    first_name =forms.CharField(max_length = 60,widget =forms.TextInput(attrs={
-        'placeholder' : 'First name',
-        'class' : 'form-control'
-        }))
-    last_name = forms.CharField(max_length = 60,widget =forms.TextInput(attrs={
-        'placeholder' : 'Last name',
-        'class' : 'form-control'
-        }))
-    password = forms.CharField(widget = forms.PasswordInput)
+    username = forms.CharField(
+        label="Username",
+        max_length=30,
+        widget=forms.TextInput(
+            attrs={"placeholder": "Username", "class": "form-control"}
+        ),
+    )
+    first_name = forms.CharField(
+        label="First name",
+        max_length=60,
+        widget=forms.TextInput(
+            attrs={"placeholder": "First name", "class": "form-control"}
+        ),
+    )
+    last_name = forms.CharField(
+        label="Last name",
+        max_length=60,
+        widget=forms.TextInput(
+            attrs={"placeholder": "Last name", "class": "form-control"}
+        ),
+    )
+    password = forms.CharField(label="Password", widget=forms.PasswordInput(attrs={"placeholder": "Password", "class": "form-control"}))
 
     def clean_username(self):
-        username = self.cleaned_data['username']
-        res = User.objects.filter(username = username)
+        username = self.cleaned_data["username"]
+        res = User.objects.filter(username=username)
         if res.count():
-            raise ValidationError(
-                    self.error_message['username_duplication']
-                    )
+            raise ValidationError(self.error_message["username_duplication"])
         return username
-    
+
     def clean_password(self):
-        password = self.cleaned_data['password']
+        password = self.cleaned_data["password"]
         re_patt_number = r"\d+"
         re_patt_letter = r"[a-zA-Z]+"
-        
+
         if len(password) < 9:
-            #logger.error('short password')
-            raise ValidationError(
-                    self.error_message['short_password']
-                    )
+            # logger.error('short password')
+            raise ValidationError(self.error_message["short_password"])
         if not re.findall(re_patt_number, password):
             raise ValidationError(
-                    self.error_message['wrong_password_format_missing_number']
-                    )
+                self.error_message["wrong_password_format_missing_number"]
+            )
         if not re.findall(re_patt_letter, password):
             raise ValidationError(
-                    self.error_message['wrong_password_format_missing_letter']
-                    )
-        #logger.error('Good password')
+                self.error_message["wrong_password_format_missing_letter"]
+            )
+        # logger.error('Good password')
         return password
 
     def save(self, commit=True):
         user = User.objects.create_user(
-                username = self.cleaned_data['username'],
-                first_name = self.cleaned_data['first_name'],
-                last_name = self.cleaned_data['last_name'],
-                password = self.cleaned_data['password']
-                )
+            username=self.cleaned_data["username"],
+            first_name=self.cleaned_data["first_name"],
+            last_name=self.cleaned_data["last_name"],
+            password=self.cleaned_data["password"],
+        )
         return user
 
 
-
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length = 30,widget =forms.TextInput(attrs={
-        'placeholder' : 'Username',
-        'class' : 'form-control'
-        }))
-    password = forms.CharField(widget = forms.PasswordInput(attrs={
-        'placeholder' : 'Your password'
-        }))
+    username = forms.CharField(
+        label="Username",
+        max_length=30,
+        widget=forms.TextInput(
+            attrs={"placeholder": "Username", "class": "form-control"}
+        ),
+    )
+    password = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput(attrs={"placeholder": "Your password", "class": "form-control"}),
+    )
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,99 +1,107 @@
-from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
-from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404, render, redirect
-from django.core.exceptions import ValidationError
-# from .models import related models
-# from .restapis import related methods
-from django.contrib.auth import login, logout, authenticate
-from django.contrib import messages
-from datetime import datetime
-import logging
 import json
+import logging
+import re
+from datetime import datetime
 
-from .forms import SignupForm
-from .forms import LoginForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib import messages
+from djangobackend.settings import BASE_DIR
+from .forms import LoginForm, SignupForm
+
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
-
 # Create your views here.
 
-
 # Create an `about` view to render a static about page
-# def about(request):
+# defabout(request):
 # ...
+def header_login_form(context):
+        form = LoginForm()
+        context["form"] = form
+
+
 def about(request):
     context = {}
-    template = 'djangoapp/about.html'
+    header_login_form(context)
+    template = "djangoapp/about.html"
     return render(request, template, context)
+
 
 def contact(request):
     context = {}
-    template = 'djangoapp/contact.html'
+    header_login_form(context)
+    template = "djangoapp/contact.html"
     return render(request, template, context)
+
 
 def login_req(request):
     context = {}
-    login_templ = 'djangoapp/login.html'
+    login_templ = "djangoapp/login.html"
 
-    if request.method == 'POST':
+    if request.method == "POST":
         form = LoginForm(request.POST)
         if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
             user = authenticate(username=username, password=password)
             if user is not None:
-                logger.error('User can log in!')
+                logger.error("User can log in!")
                 login(request, user)
-                return redirect('djangoapp:index')
+                return redirect("djangoapp:index")
             else:
-                logger.error('User can NOT  log in!')
-                context['message'] = 'Invalid username or password!'
-                context['form'] = form
+                logger.error("User can NOT  log in!")
+                messages.error(request, "Invalid username or password!")
+                context["form"] = form
                 return render(request, login_templ, context)
         else:
-            logger.error('Invalid input!')
-            context['message'] = 'Invalid user input!'
-            context['form'] = form
+            logger.error("Invalid input!")
+            messages.error(request, "Invalid user input!")
+            context["form"] = form
             return render(request, login_templ, context)
-    elif request.method == 'GET':
-            context['form'] = LoginForm() 
-            return render(request, login_templ, context)
+    elif request.method == "GET":
+        context["form"] = LoginForm()
+        return render(request, login_templ, context)
 
 
 def logout_req(request):
     logout(request)
-    return redirect('djangoapp:index')
+    return redirect("djangoapp:index")
+
 
 def registrate_req(request):
     context = {}
-    registration_templ = 'djangoapp/registration.html'
-    if request.method == 'GET':
-        context['form'] = SignupForm()
+    registration_templ = "djangoapp/registration.html"
+    if request.method == "GET":
+        logger.error(BASE_DIR)
+        context["form"] = SignupForm()
         return render(request, registration_templ, context)
-    elif request.method == 'POST':
-        form = SignupForm(request.POST) 
+    elif request.method == "POST":
+        form = SignupForm(request.POST)
         if form.is_valid():
             try:
-                is_user = False
-                logger.error('form has been valid')
-                user = form.save()
+                logger.error("form has been valid")
+                user = form.save() 
                 login(request, user)
-                logger.error('User is created!')
-                messages.success = 'The form was valid, user is registered' 
-                return redirect('djangoapp:index')
+                logger.error("User is created!")
+                messages.success(request, "Succsesful registration!")
+                return redirect("djangoapp:index")
             except ValidationError as err:
                 logger.error(form.error_message)
         else:
-            context['form'] = form
-            messages.success = 'The form was not valid' 
-            return render(request, registration_templ, context)
-    
-#  Create a `contact` view to return a static contact page
-#def contact(request):
+                context["form"] = form
+                messages.error(request, "The form is invalid")
+                return render(request, registration_templ, context)
 
-# Create a `login_request` view to handle sign in request
+
+#  Create a `contact` view to return a static contact page
+# def contact(request):
+
 # def login_request(request):
 # ...
 
@@ -105,13 +113,15 @@ def registrate_req(request):
 # def registration_request(request):
 # ...
 
-# Update the `get_dealerships` view to render the index page with a list of dealerships
+# Update the `get_dealerships` view to render the index page with a list
+# of dealerships
+
+
 def get_dealerships(request):
     context = {}
     if request.method == "GET":
-        form = LoginForm()
-        context['form'] = form
-        return render(request, 'djangoapp/index.html', context)
+        header_login_form(context)
+        return render(request, "djangoapp/index.html", context)
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
@@ -121,4 +131,3 @@ def get_dealerships(request):
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
 # ...
-
